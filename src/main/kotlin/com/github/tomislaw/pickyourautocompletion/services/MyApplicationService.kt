3aec09634bcc5fd01b5ed7dea527afc644795a2a
GitHub Action <action@github.com>
2021-12-22 07:48:57 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/tomislaw/pickyourautocompletion/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.tomislaw.pickyourautocompletion.services
+
+import com.github.tomislaw.pickyourautocompletion.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/tomislaw/pickyourautocompletion/services/MyProjectService.kt
+++ src/main/kotlin/com/github/tomislaw/pickyourautocompletion/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.tomislaw.pickyourautocompletion.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tomislaw.pickyourautocompletion.MyBundle
 
 class MyProjectService(project: Project) {
 

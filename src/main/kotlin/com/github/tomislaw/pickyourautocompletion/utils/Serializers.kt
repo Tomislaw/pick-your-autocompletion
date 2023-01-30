ea7f6012d1f8cfd23e5bf48dabc8835f93011714
Tomasz Staniewski <tomislaw2.0@gmail.com>
2023-01-30 13:41:06 +0100

    Updated plugin versions Removed deprecated functions

--- src/main/kotlin/com/github/tomislaw/pickyourautocompletion/utils/Serializers.kt
+++ src/main/kotlin/com/github/tomislaw/pickyourautocompletion/utils/Serializers.kt
@@ -1,6 +1,6 @@
 package com.github.tomislaw.pickyourautocompletion.utils
 
-import com.intellij.util.Base64
+import java.util.Base64
 import com.intellij.util.xmlb.Converter
 import java.nio.charset.Charset
 
@@ -13,15 +13,16 @@ class CharsetSerializer : Converter<Charset>() {
 class Base64HeaderListSerializer : Converter<MutableList<Pair<String, String>>>() {
     override fun toString(value: MutableList<Pair<String, String>>): String =
         value.joinToString(separator = ",") {
-            Base64.encode(it.first.toByteArray()) + ":" + Base64.encode(it.second.toByteArray())
+            Base64.getEncoder().encodeToString(it.first.toByteArray()) + ":" + Base64.getEncoder()
+                .encodeToString(it.second.toByteArray())
         }
 
     override fun fromString(value: String): MutableList<Pair<String, String>> =
         value.split(",").map {
             it.split(":").let { pair ->
                 Pair(
-                    Base64.decode(pair[0]).decodeToString(),
-                    Base64.decode(pair[1]).decodeToString()
+                    Base64.getDecoder().decode(pair[0]).decodeToString(),
+                    Base64.getDecoder().decode(pair[1]).decodeToString()
                 )
             }
 
@@ -30,8 +31,8 @@ class Base64HeaderListSerializer : Converter<MutableList<Pair<String, String>>>(
 }
 
 class Base64StringSerializer : Converter<String>() {
-    override fun toString(value: String): String = Base64.encode(value.toByteArray())
+    override fun toString(value: String): String = Base64.getEncoder().encodeToString(value.toByteArray())
 
-    override fun fromString(value: String): String = Base64.decode(value).decodeToString()
+    override fun fromString(value: String): String = Base64.getDecoder().decode(value).decodeToString()
 
 }
